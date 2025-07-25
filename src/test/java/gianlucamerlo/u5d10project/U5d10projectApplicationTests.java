cb33d35ea9e9ec9ad416f01b54cfc69a38b2568e
gianlucamerlo97 <notoriousbig9700@gmail.com>
2025-07-25 10:00:40 +0200

    Initial commit

---
+++ src/test/java/gianlucamerlo/u5d10project/U5d10projectApplicationTests.java
@@ -0,0 +1,13 @@
+package gianlucamerlo.u5d10project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5d10projectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
